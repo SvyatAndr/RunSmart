232927f5b89ba22ba27be9a316f008e53bff557a
SvyatAndr <chundzhy@gmail.com>
2023-07-14 11:12:47 +0300

    fix btn carousel 2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
 		// adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="/icon/slick_btn/left.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="/icon/slick_btn/right.png"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icon/slick_btn/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icon/slick_btn/right.png"></button>',
 		responsive: [
 			{
 				breakpoint: 992,
