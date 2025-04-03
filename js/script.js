f65ec0c9c5a76900e36c9dc25a39228275427f7f
Svyat <svyatandriua@gmail.com>
2025-04-03 17:42:50 +0300

    Added button pageup

--- js/script.js
+++ js/script.js
@@ -130,3 +130,19 @@ $('form').submit(function (e) {
 	});
 	return false;
 });
+
+// Smooth scroll and pageup
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1200) {
+		$('.pageup').fadeIn();
+	} else {
+		$('.pageup').fadeOut();
+	}
+});
+
+$("a[href^='#']").click(function () {
+	const _href = $(this).attr("href");
+	$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+	return false;
+});
